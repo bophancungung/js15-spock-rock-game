349b1b5b14d4e0e0b68a5177a567816720a102d7
bophancungung <bophancungung@gmail.com>
2021-08-13 17:03:51 +0700

    Js setup player selection

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
+const playerScoreEl = document.getElementById('player-score');
+const playerChoiceEl = document.getElementById('player-choice');
+const computerScoreEl = document.getElementById('computer-score');
+const computerChoiceEl = document.getElementById('computer-choice');
+const resultText = document.getElementById('result-text');
+
+const playerRock = document.getElementById('player-rock');
+const playerPaper = document.getElementById('player-paper');
+const playerScissors = document.getElementById('player-scissors');
+const playerLizard = document.getElementById('player-lizard');
+const playerSpock = document.getElementById('player-spock');
+
+const allGameIcons = document.querySelectorAll('.far');
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
@@ -8,3 +21,37 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+// Reset all 'selected' icons
+function resetSelected() {
+  allGameIcons.forEach(icon => {
+    icon.classList.remove('selected');
+  })
+}
+
+// Passing player selection value and styling icons
+function select(playerChoice) {
+  resetSelected();
+  // Add 'selected' styling & playerChoice
+  switch(playerChoice) {
+    case 'rock':
+      playerRock.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      playerPaper.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+      playerScissors.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Scissors';
+      break;
+    case 'lizard':
+      playerLizard.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Lizard';
+      break;
+    case 'spock':
+      playerSpock.classList.add('selected');
+      playerChoiceEl.textContent = ' --- Spock';
+      break;
+  }
+}
